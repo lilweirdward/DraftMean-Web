b13a63ec2a236e91ff72df6c731fb8dbfb511c27
Zach Woodward <zachw38@gmail.com>
2018-04-07 20:00:52 -0400

    added login to menu to navigate to boards

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, DoCheck } from '@angular/core';
+import { Component, OnInit, DoCheck, HostListener } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -8,5 +9,22 @@ import { Component, OnInit, DoCheck } from '@angular/core';
 export class AppComponent {
   
   constructor() { }
+
+  loginVisible = false;
+  boardId = "";
+
+  displayLogin() {
+    this.loginVisible = !this.loginVisible;
+  }
+
+  updateBoardId(boardId: string) {
+    this.boardId = boardId;
+  }
+
+  // @HostListener('click')
+  // loginToBoard(boardId, teamName) {
+  //   this.router.navigateByUrl(`/board/${boardId}`);
+  //   return true;
+  // }
 
 }
